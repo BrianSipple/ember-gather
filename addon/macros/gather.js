39f3d753ca4110ee9654c5d4521197e168cc5bad
BrianSipple <bsipple57@gmail.com>
2016-10-06 02:28:09 -0700

    implement top-level computed functionality for gather and deepGather

--- addon/macros/gather.js
+++ addon/macros/gather.js
@@ -1,9 +1,17 @@
 import computed from 'ember-computed';
+import get from 'ember-metal/get';
+import { A, isEmberArray as isArray } from 'ember-array/utils';
 
 export default function gather(...dependentKeys) {
   const computedFunc = computed({
     get() {
+      return A(
+        dependentKeys.reduce((accumulatedList, currentKey) => {
+          const value = get(this, currentKey);
 
+          return isArray(value) ? [...accumulatedList, ...value] : [...accumulatedList, value];
+        }, [])
+      );
     }
   });
 
